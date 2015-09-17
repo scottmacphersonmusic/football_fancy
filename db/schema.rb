1e1b4e2f3a63e9735fb6307ac04f1c37ba1b4074
Scott Macpherson <scottmacphersonmusic@gmail.com>
2015-09-17 12:47:33 -0700

    Add topics_users relation to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917074346) do
+ActiveRecord::Schema.define(version: 20150917192738) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20150917074346) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "topics_users", id: false, force: :cascade do |t|
+    t.integer "topic_id"
+    t.integer "user_id"
+  end
+
+  add_index "topics_users", ["topic_id"], name: "index_topics_users_on_topic_id"
+  add_index "topics_users", ["user_id"], name: "index_topics_users_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
